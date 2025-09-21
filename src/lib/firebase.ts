14b1b5af75ef39df8841a5e2d4075f7e3f4d4297
N.Nagaraju c 99 <nallaboinanagaraju3@gmail.com>
2025-09-21 12:12:26 +0000

    Make this website deploy ready for netlify

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,22 +1,27 @@
 'use client';
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
+import { initializeApp, getApps } from 'firebase/app';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD6kT1AfOj7WqtcNKO4USnUcjo1BCiAgjA",
-  authDomain: "studio-363859993-44900.firebaseapp.com",
-  projectId: "studio-363859993-44900",
-  storageBucket: "studio-363859993-44900.firebasestorage.app",
-  messagingSenderId: "757018108861",
-  appId: "1:757018108861:web:eca1e8625105fafd59e8d4"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+let app;
+if (!getApps().length) {
+  app = initializeApp(firebaseConfig);
+} else {
+  app = getApps()[0];
+}
+
+export { app };
